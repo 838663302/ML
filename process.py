34cdd13eae5240487a1d333ad8865e0b768f7459
azt1szh <tao.zhang4@cn.bosch.com>
2026-07-22 16:02:44 +0800

    feat: add abstract Tokenizer with ZHTokenizer/ENTokenizer; refactor process.py to use Tokenizer class

--- process.py
+++ process.py
@@ -2,9 +2,11 @@ import pandas as pd
 import config
 from sklearn.model_selection import train_test_split
 import jieba
+from tokenizer import ZHTokenizer
 
-def build_dataset(sentences, word2id):
-    indexed = [[word2id.get(token, 0) for token in jieba.lcut(sentence)] for sentence in sentences]
+def build_dataset(sentences, tokenizer):
+    # indexed = [[word2id.get(token, 0) for token in Tokenizer.tokenize(sentence)] for sentence in sentences]
+    indexed = [tokenizer.encode(sentence) for sentence in sentences]
     dataset = []
     for data in indexed:
         for i in range(len(data) - config.WINDOW_SIZE):
@@ -24,22 +26,25 @@ def process():
             sentences.append(s.split("：")[1].strip())
     train_data, test_data = train_test_split(sentences, test_size=0.2, random_state=42)
 
-    vocab_set = set()
-    for sentence in train_data:
-        vocab_set.update(jieba.cut(sentence))
-    
-    vocab_list = ["<unk>"] + list(vocab_set)
-    word2id = {word: i for i, word in enumerate(vocab_list)}
-    id2word = {i: word for i, word in enumerate(vocab_list)}
-
     vocab_path = config.DATASET_DIR / "vocab.txt"
-    with open(vocab_path, "w", encoding="utf-8") as f:
-        for token in vocab_list:
-            f.write(token + "\n")
+    # vocab_set = set()
+    # for sentence in train_data:
+    #     vocab_set.update(jieba.cut(sentence))
+    
+    # vocab_list = ["<unk>"] + list(vocab_set)
+    # word2id = {word: i for i, word in enumerate(vocab_list)}
+    # id2word = {i: word for i, word in enumerate(vocab_list)}
+
+    
+    # with open(vocab_path, "w", encoding="utf-8") as f:
+    #     for token in vocab_list:
+    #         f.write(token + "\n")
+    ZHTokenizer.build_vocab(train_data, vocab_path)
+    tokenizer = ZHTokenizer.from_vocab(vocab_path)
 
     # print(sentences[0:10])
-    train_dataset = build_dataset(train_data, word2id)
-    test_dataset = build_dataset(test_data, word2id)
+    train_dataset = build_dataset(train_data, tokenizer)
+    test_dataset = build_dataset(test_data, tokenizer)
 
     train_dataset_path = config.DATASET_DIR / "train_data_set.jsonl"
     pd.DataFrame(train_dataset).to_json(train_dataset_path, orient="records", lines=True, force_ascii=False)
---
+++ tokenizer.py
@@ -0,0 +1,53 @@
+from abc import ABC, abstractmethod
+import jieba
+
+class Tokenizer(ABC):
+    oov = "<unk>"
+    pad = "<pad>"
+    sos = "<sos>"
+    eos = "<eos>"
+    def __init__(self, vocab_list):
+        specials = [self.oov, self.pad, self.sos, self.eos]
+        self.word2id = {word: i for i, word in enumerate(vocab_list)}
+        self.id2word = {i: word for i, word in enumerate(vocab_list)}
+        self.vocab_size = len(vocab_list)
+        self.oov_id = self.word2id[self.oov]
+        self.pad_id = self.word2id[self.pad]
+        self.sos_id = self.word2id[self.sos]
+        self.eos_id = self.word2id[self.eos]
+
+    @classmethod
+    @abstractmethod
+    def tokenize(cls, sentence):
+        """子类必须实现此方法，定义分词逻辑"""
+        pass
+
+    def encode(self, sentence):
+        return [self.word2id.get(token, self.oov_id) for token in self.tokenize(sentence)]
+    
+    @classmethod
+    def build_vocab(cls, sentences, path):
+        vocab_set = set()
+        for sentence in sentences:
+            vocab_set.update(cls.tokenize(sentence))
+        vocab_list = [cls.oov, cls.pad, cls.sos, cls.eos] + list(vocab_set)
+        with open(path, "w", encoding="utf-8") as f:
+            f.write("\n".join(vocab_list))
+    
+    @classmethod
+    def from_vocab(cls, path):
+        with open(path, "r", encoding="utf-8") as f:
+            vocab_list = f.read().splitlines()
+        return cls(vocab_list)
+        
+
+class ENTokenizer(Tokenizer):
+    @classmethod
+    def tokenize(cls, sentence):
+        from nltk import word_tokenize
+        return word_tokenize(sentence)
+
+class ZHTokenizer(Tokenizer):
+    @classmethod
+    def tokenize(cls, sentence):
+        return jieba.lcut(sentence)
